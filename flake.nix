44a6ee8970900464b5986db4f1e61d9913919f69
Simon Guest <simon.guest@tesujimath.org>
2026-02-02 15:39:42 +1300

    Extracted Python bindings from main repo (#1)

--- flake.nix
+++ flake.nix
@@ -37,7 +37,6 @@
               cargo-outdated
               cargo-edit
               gcc
-              gdb
               rust-analyzer
               rust-bin.stable.latest.default
               rustfmt
